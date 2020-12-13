27454354a7ae396a9e5ad3f15dcda942d9dc746a
Gord Bond <gordbondartist@gmail.com>
2020-12-13 15:40:16 -0500

    mobile menu changed to humburger menu

--- navScripts.js
+++ navScripts.js
@@ -22,3 +22,12 @@ for(var i=0; i< buttons.length; i++){
         }
     })
 }
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
